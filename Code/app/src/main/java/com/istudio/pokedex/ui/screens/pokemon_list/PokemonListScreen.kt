0401793c52f212fab2fc959e4af694f3102c30a2
Devrath <devrath.dev595@gmail.com>
2023-01-26 15:37:06 +0530

    reformatted code

--- Code/app/src/main/java/com/istudio/pokedex/ui/screens/pokemon_list/PokemonListScreen.kt
+++ Code/app/src/main/java/com/istudio/pokedex/ui/screens/pokemon_list/PokemonListScreen.kt
@@ -1,34 +1,15 @@
 package com.istudio.pokedex.ui.screens.pokemon_list
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.istudio.pokedex.R
 import com.istudio.pokedex.ui.screens.pokemon_list.composables.PokemonBanner
 import com.istudio.pokedex.ui.screens.pokemon_list.composables.PokemonSearch
 
