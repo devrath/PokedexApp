b79ead6918daffcce3a7bbb8b1b5ec693e70d7dd
Devrath <devrath.dev595@gmail.com>
2023-01-31 20:03:32 +0530

    error handeling added

--- Code/app/src/main/java/com/istudio/pokedex/ui/screens/pokemon_list/composables/PokemonList.kt
+++ Code/app/src/main/java/com/istudio/pokedex/ui/screens/pokemon_list/composables/PokemonList.kt
@@ -2,10 +2,22 @@ package com.istudio.pokedex.ui.screens.pokemon_list.composables
 
 import android.graphics.drawable.Drawable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
@@ -24,6 +36,7 @@ fun PokemonLazyList(
         state = state
     ){
 
+
         items(pokemonList.itemSnapshotList.items.size) { position ->
             PokemonListItem(
                 item=pokemonList.itemSnapshotList.items[position],
@@ -31,25 +44,82 @@ fun PokemonLazyList(
             )
         }
 
-        when (val state = pokemonList.loadState.refresh) { //FIRST LOAD
-            is LoadState.Error -> {
-                //TODO Error Item
-                //state.error to get error message
+        pokemonList.apply {
+            when {
+                loadState.refresh is LoadState.Loading -> {
+                    item { LoadingView(modifier = Modifier.fillParentMaxSize()) }
+                }
+                loadState.append is LoadState.Loading -> {
+                    item { LoadingItem() }
+                }
+                loadState.refresh is LoadState.Error -> {
+                    val e = pokemonList.loadState.refresh as LoadState.Error
+                    item {
+                        ErrorItem(
+                            message = e.error.localizedMessage!!,
+                            modifier = Modifier.fillParentMaxSize(),
+                            onClickRetry = { retry() }
+                        )
+                    }
+                }
+                loadState.append is LoadState.Error -> {
+                    val e = pokemonList.loadState.append as LoadState.Error
+                    item {
+                        ErrorItem(
+                            message = e.error.localizedMessage!!,
+                            onClickRetry = { retry() }
+                        )
+                    }
+                }
             }
-            is LoadState.Loading -> { // Loading UI
-                //
-            }
-            else -> {}
         }
+    }
+}
 
-        when (val state = pokemonList.loadState.append) { // Pagination
-            is LoadState.Error -> {
-                //state.error to get error message
-            }
-            is LoadState.Loading -> { // Pagination Loading UI
 
-            }
-            else -> {}
+@Composable
+fun LoadingView(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun LoadingItem() {
+    CircularProgressIndicator(
+        modifier = Modifier.fillMaxWidth()
+            .padding(16.dp)
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+}
+
+
+@Composable
+fun ErrorItem(
+    message: String,
+    modifier: Modifier = Modifier,
+    onClickRetry: () -> Unit
+) {
+    Row(
+        modifier = modifier.padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = message,
+            maxLines = 1,
+            modifier = Modifier.weight(1f),
+            style = MaterialTheme.typography.h6,
+            color = Color.Red
+        )
+        OutlinedButton(onClick = onClickRetry) {
+            Text(text = "Try again")
         }
     }
 }
