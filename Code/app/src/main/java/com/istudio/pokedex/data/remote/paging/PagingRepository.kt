807f21ec6571b0ed2c2843264abe2296e85daa9f
Devrath <devrath.dev595@gmail.com>
2023-02-01 00:07:02 +0530

    error handeling added

--- Code/app/src/main/java/com/istudio/pokedex/data/remote/paging/PagingRepository.kt
+++ Code/app/src/main/java/com/istudio/pokedex/data/remote/paging/PagingRepository.kt
@@ -3,12 +3,16 @@ package com.istudio.pokedex.data.remote.paging
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import com.istudio.pokedex.data.remote.PokeApi
+import com.istudio.pokedex.util.Constants.PAGE_SIZE
 import javax.inject.Inject
 
 class PagingRepository @Inject constructor(
     private val api: PokeApi
 ) {
-    fun getPokemon() = Pager(config = PagingConfig(pageSize = 20,),
-        pagingSourceFactory = { PokemonPagingSource(api) }
+    fun getPokemon() = Pager(
+        config = PagingConfig(pageSize = PAGE_SIZE),
+        pagingSourceFactory = {
+            PokemonPagingSource(api)
+        }
     ).flow
 }
